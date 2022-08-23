2a33423b0425f69562ea6c19cee51cc1f84d7fcc
Aleks <shapovalov.alex.v@gmail.com>
2022-08-23 15:49:24 +0300

    task_2

--- Homework_1/task_2.cs
+++ Homework_1/task_2.cs
@@ -1,4 +1,18 @@
-// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
-// 2, 3, 7 -> 7
-// 44 5 78 -> 78
-// 22 3 9 -> 22
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Введите первое число: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numberB = int.Parse(Console.ReadLine());
+ 
+    if(numberA > numberB)
+{
+    Console.WriteLine("Первое число больше второго");
+}
+    else if (numberA < numberB)
+{
+    Console.WriteLine("Первое число меньше второго");
+}
