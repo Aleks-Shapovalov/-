4d869c1d8f56be46058d10ba988740f0b9586bf2
Aleks <shapovalov.alex.v@gmail.com>
2022-08-24 10:16:18 +0300

    task_8

--- Homework_1/Task_8/Program.cs
+++ Homework_1/Task_8/Program.cs
@@ -2,3 +2,9 @@
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
 
+Console.Write("Введите число для проверки: ");
+int numberA = int.Parse (Console.ReadLine());
+
+for (int x = 1; x < numberA; x++)       
+if (x%2==0)            
+Console.WriteLine(x);
