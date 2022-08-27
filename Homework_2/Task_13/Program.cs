4530c1ce754e24a8a98ac71c59982198b06d5294
Aleks <shapovalov.alex.v@gmail.com>
2022-08-27 10:43:03 +0300

    task_13

---
+++ Homework_2/Task_13/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите любое трехзначное число: ");
+int numberA = int.Parse(Console.ReadLine());
+
+if (numberA < 100)
+{
+    Console.WriteLine($"третьей цифры нет");
+}
+
+else
+{
+    int num1 = (numberA % 100)%10;
+    Console.WriteLine(num1);
+}
