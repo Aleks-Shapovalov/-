46066092ab714532ee10a1639b676b30cde3149f
Aleks <shapovalov.alex.v@gmail.com>
2022-09-19 19:30:26 +0300

    task_56

--- Homework_8/Task_56/Program.cs
+++ Homework_8/Task_56/Program.cs
@@ -5,3 +5,49 @@
 // 8 4 2 4
 // 5 2 6 7
 // Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+Console.WriteLine("Задаем количество строк и столбцов нашего КВАДРАТНОГО массива: ");
+int Massive = Convert.ToInt32(Console.ReadLine());
+int[,] numbers = new int[Massive, Massive];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+int MinimumSum = Int32.MaxValue;
+int index = 0;
+
+for (int i = 0; i < numbers.GetLength(0); i++)
+{
+    int sum = 0;
+    for (int j = 0; j < numbers.GetLength(1); j++)
+    {
+        sum = sum + numbers[i, j];
+    }
+    if (sum < MinimumSum)
+    {
+        MinimumSum = sum;
+        index++;
+    }
+}
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
+Console.WriteLine("строка с наименьшей суммой елементов под номером: " + (index) + ", с суммой елементов равной: " + (MinimumSum));
